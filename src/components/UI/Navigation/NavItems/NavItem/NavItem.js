6533f69afc5e5967c8591e981ebbed2cfac7af7f
hossamelneily <hossamelneily@gmail.com>
2020-02-10 09:29:06 +0300

    Adding some missing points from the Routing

--- src/components/UI/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/UI/Navigation/NavItems/NavItem/NavItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 import classes from './NavItem.module.css'
-
+import {Link,NavLink} from "react-router-dom";
+import Orders from "../../../../Order/Order";
 
 const Navitem=(props)=>{
     console.log(props.active)
     return(
 
     <li className={classes.NavItem}>
-        <a className={props.active?classes.active:null} href='/'>
+        <NavLink exact activeClassName={classes.active} to={{
+            pathname:props.link
+        }}>
             {props.children}
-        </a>
+        </NavLink>
     </li>
     )
 }
